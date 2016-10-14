37c37805b988709c6bcbc307e5708af0f28d0a52
Matas Razgunas <matas.razgunas@gmail.com>
2016-10-14 16:34:31 +0300

    Add RC input timeout and MIN MAX range

--- src/drivers/rc_input.c
+++ src/drivers/rc_input.c
@@ -3,12 +3,33 @@
 
 #include "rc_input.h"
 
+#define RC_MAX 2400
+#define RC_MIN 800
+
+static virtual_timer_t rc_timeout;
+
 icucnt_t last_width, last_period;
 static bool init = false;
 
-static void icuwidthcb(ICUDriver *icup) {
+/*
+ * RC timeout timer callback.
+ */
+static void rc_timeout_cb(void *arg) {
+    (void) arg;
+    last_width = 0;
+}
 
-  last_width = icuGetWidthX(icup);
+static void icuwidthcb(ICUDriver *icup) {
+    uint16_t width = icuGetWidthX(icup);
+
+    if(width < RC_MAX || last_width > RC_MIN)
+        last_width = width;
+
+    /* Set timeout virtual timer if we don't get more callback
+    * int given time it will set rpm to 0*/
+    chSysLockFromISR();
+    chVTSetI(&rc_timeout, MS2ST(200), rc_timeout_cb, NULL);
+    chSysUnlockFromISR();
 }
 
 static void icuperiodcb(ICUDriver *icup) {
@@ -30,6 +51,9 @@ void init_rc_input(void) {
     icuStart(&ICUD8, &icucfg);
     icuStartCapture(&ICUD8);
     icuEnableNotifications(&ICUD8);
+
+    /* RPM timeout timer initialization.*/
+    chVTObjectInit(&rc_timeout);
 
     init = true;
 
